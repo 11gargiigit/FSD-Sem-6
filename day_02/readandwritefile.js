c256f3247d7945bef4e74199f84e704eb93d2e65
Gargi Singh <11gargisingh@gmail.com>
2026-02-09 13:06:42 +0530

    second

---
+++ day_02/readandwritefile.js
@@ -0,0 +1,17 @@
+import fs from 'fs';
+//import { fileURLToPath } from 'url';
+//console.log(fs);
+const writeFileSync = (file,data) => {
+    try{
+   fs.writeFileSync(file , data);
+    console.log("file has been written successfully");
+    }
+    catch(error){
+        console.log("some error occured");
+    }
+    
+}
+writeFileSync ("./example.txt" , "this data has been written through sync function 1");
+writeFileSync ("./example.txt" , "this data has been written through sync function 2");
+writeFileSync ("./example.txt" , "this data has been written through sync function 3");\
+
